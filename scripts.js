dadc975e0feb8d606abf63dd63e7be5b6305d08b
krzysx99 <gta09848lks@gmail.com>
2025-12-09 17:20:42 +0100

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const imgs = document.querySelectorAll('.gallery img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+const closeBtn = document.getElementById('close');
+
+imgs.forEach(img => {
+    img.addEventListener('click', () => {
+        lightbox.style.display = 'flex';
+        lightboxImg.src = img.src;
+    });
+});
+
+closeBtn.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+});
+
+// zamknij po kliknięciu poza obrazem
+lightbox.addEventListener('click', (e) => {
+    if (e.target === lightbox) lightbox.style.display = 'none';
+});
